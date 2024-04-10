8b8cce81e263a413087b48d4e9922c58fdc7c1bd
El <elmalkiziad@gmail.com>
2024-04-10 14:44:08 +0200

    Script to download CycPeptMPDB

---
+++ src/ai4chemistry/CycPept_Download.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+
+import urllib.request
+
+# URL of the CycPeptDB database
+url = "http://cycpeptmpdb.com/static//download/peptides/CycPeptMPDB_Peptide_All.csv"
+
+# Destination directory
+destination_dir = "/Users/ziadelmalki/Desktop/ai4chem/docs/data"
+
+# Create the destination directory if it doesn't exist
+os.makedirs(destination_dir, exist_ok=True)
+
+# Download the database file
+urllib.request.urlretrieve(url, os.path.join(destination_dir, "cycpeptdb.csv"))
+
+print("CycPeptDB downloaded is complete, you can find it stored in docs/data directory.")
