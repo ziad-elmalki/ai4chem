617d07ecbc55ae62876a7203ad8019dfecf44275
El <elmalkiziad@gmail.com>
2024-04-23 13:27:42 +0200

    Change the data generation from a pickle to a h5py file

--- src/ai4chemistry/PepNet_DataGenerator.py
+++ src/ai4chemistry/PepNet_DataGenerator.py
@@ -1,42 +1,39 @@
 import numpy as np 
-import matplotlib.pyplot as plt
 import pandas as pd
-from PIL import Image
-import pickle 
 from rdkit import Chem
 from rdkit.Chem import Draw
+import h5py
 
+if __name__ == '__main__':
+    # Read the cycpepdb_clean as a dataframe
+    df = pd.read_csv('../../docs/data/cycpeptdb_clean.csv')
 
-# Read the cycpepdb_clean as a dataframe
-df = pd.read_csv('../../docs/data/cycpeptdb_clean.csv')
+    PepNet_data = pd.DataFrame(columns=['Permeability', 'Image'])
 
-PepNet_data = pd.DataFrame(columns=['Permeability', 'Image'])
+    # Create a new column 'images' to store the RDKit images of the peptides
+    PepNet_data['Image'] = df['SMILES'].apply(lambda x: Draw.MolToImage(Chem.MolFromSmiles(x)))
 
-# Create a new column 'images' to store the RDKit images of the peptides
-PepNet_data['Image'] = df['SMILES'].apply(lambda x: Draw.MolToImage(Chem.MolFromSmiles(x)))
+    #Create column Permeability and store the permeability values
+    PepNet_data['Permeability'] = df['Permeability']
 
-#Create column Permeability and store the permeability values
-PepNet_data['Permeability'] = df['Permeability']
+    # Define the shape of the image dataset
+    nfiles = len(PepNet_data)
+    IMG_WIDTH = PepNet_data['Image'][0].size[0]
+    IMG_HEIGHT = PepNet_data['Image'][0].size[1]
 
-# Save the data to a Pickle file
-PepNet_data.to_pickle('../../docs/data/PepNet_data.pkl')
+    # Create an h5 file
+    h5file = '../../docs/data/PepNet_data.h5'
 
-# Load the data from the Pickle file
-PepNet_data = pd.read_pickle('../../docs/data/PepNet_data.pkl')
-
-# Visualize the first few samples from the DataFrame
-num_samples_to_visualize = 5
-
-for i in range(num_samples_to_visualize):
-    # Get the image data
-    image_data = PepNet_data.loc[i, 'Image']
-    
-    # Get the permeability value
-    permeability_value = PepNet_data.loc[i, 'Permeability']
-    
-    # Visualize the image and permeability value
-    plt.figure(figsize=(4, 4))
-    plt.imshow(image_data)
-    plt.title(f'Permeability Value: {permeability_value}')
-    plt.axis('off')
-    plt.show()
+    with h5py.File(h5file, 'w') as h5f:
+        # Create the image dataset
+        img_ds = h5f.create_dataset('images', shape=(nfiles, IMG_WIDTH, IMG_HEIGHT, 3), dtype=int)
+        
+        # Save the images to the dataset
+        for i in range(nfiles):
+            img_ds[i] = np.array(PepNet_data['Image'][i])
+        
+        # Create the permeability dataset
+        perm_ds = h5f.create_dataset('permeability', shape=(nfiles,), dtype=float)
+        
+        # Save the permeability values to the dataset
+        perm_ds[:] = PepNet_data['Permeability']
