a779732e4262f0a55b306c2f7ae3914a90720ab8
Leencol <colleen.lonfat@epfl.ch>
2024-05-29 19:24:24 +0200

    bug fixed

--- src/ai4chemistry/Notebooks/Baseline Model/Molecule_Fingerprint.py
+++ src/ai4chemistry/Notebooks/Baseline Model/Molecule_Fingerprint.py
@@ -13,6 +13,6 @@ df = pd.read_csv('../../../../docs/data/cycpeptdb_clean.csv')
 df['MolFromSmiles'] = df['SMILES'].apply(Chem.MolFromSmiles)
 df['fingerprint'] = MAP4.calculate_many(df['MolFromSmiles'])
 
-pd.to_csv('../../../../docs/data/cycpeptdb_clean_fps.csv')
+df.to_csv('../../../../docs/data/cycpeptdb_clean_fps.csv')
 
 #creation of a code to add the 'fingerprint' feature to the database cycpepcycpeptdb_clean_fps.csv
